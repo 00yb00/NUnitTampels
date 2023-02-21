8fd04889f809ed26ac75bc9b2f314ce068dcf6f0
yaffa <boaronyaffa@gmail.com>
2023-02-21 17:40:11 +0200

    Add project files.

---
+++ mainProg/Items.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mainProg
+{
+    public class Items
+    {
+        public Dictionary<string, Item> list { get; set; }
+        public Items()
+        {
+            list= new Dictionary<string, Item>();   
+        }
+        public void run()
+        {
+            list.Add("a", new Item(){ Id = 1 ,Name="banana"}) ;
+            list.Add("b", new Item() { Id = 2, Name = "apple" });
+            list.Add("c", new Item() { Id = 3, Name = "orange" });
+        }
+        public int Calc(string itemName,int count)
+        {
+            Item item =list[itemName];
+            return item.Price*count;
+        }
+        public int Plus(int a,int b)
+        {
+            return a+b; 
+        }
+        public int Minus(int a, int b)
+        {
+            return a - b;
+        }
+    }
+}
---
+++ test mainProg/TestItem.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using mainProg;
+using NUnit.Framework;
+namespace test_mainProg
+{
+    [TestFixture]
+    public class TestItem
+    {
+        [Test]
+        //this function run first
+        [SetUp]
+        public void Init()
+        {
+
+        }
+        [Test]
+        [Order(2)]
+        //check second
+        public void TestPlus()
+        {
+            Items item = new Items();
+            int ret = item.Plus(5, 5);
+            // if ret ==10 return sucsess else fail
+            Assert.AreEqual(10, ret);    
+        }
+        [Test]
+        //catgory for the test 
+        [Category("check function")]
+        //i choose what it order first
+        //check first
+        [Order(1)]
+        public void TestMinus()
+        {
+            Items item = new Items();
+            int ret = item.Minus(5, 5);
+
+            Assert.AreEqual(0, ret,"result 0");
+        }
+        // Ignore from this function( not run)
+        [Test,Ignore("this function not finished yet")]
+        public void TestSomesing()
+        {
+
+        }
+    }
+}
